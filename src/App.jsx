b90fcbb67a798b827affdc994a7af8d0bd9069f2
hoyewole <44290991+hoyewole@users.noreply.github.com>
2023-10-22 20:39:36 +0100

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 // App.js
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link, NavLink, useLocation  } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Link, NavLink, useLocation, HashRouter  } from 'react-router-dom';
 import CustomCounterPage from './pages/CustomCounterPage';
 import NotFound from './pages/NotFound';
 import ErrorBoundaryTest from './pages/ErrorBoundaryTest';
@@ -16,7 +16,7 @@ function App() {
   return (
     <>
       <Container>
-        <Router>
+        <HashRouter>
             <Navbar expand="lg" bg="dark" data-bs-theme="dark" className="bg-body-tertiary p-4">
               <Navbar.Brand href="/"><h2>Counter App</h2></Navbar.Brand>
               <Navbar.Toggle aria-controls="basic-navbar-nav" />
@@ -34,6 +34,18 @@ function App() {
                 </Nav>
               </Navbar.Collapse>
             </Navbar>
+
+            <Nav variant="underline" className="justify-content-end" style={{width: '100%'}}>
+              <Nav.Item>
+                <Nav.Link as={Link} to="/CounterApp/" className={location.pathname === '/CounterApp/' ? 'active' : ''}>Custom Counter</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link as={Link} to="/CounterApp/error-boundary" className={location.pathname === '/CounterApp/error-boundary' ? 'active' : ''}>Error Boundary Test</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link as={Link} to="/CounterApp/my-error-page" className={location.pathname === '/CounterApp/my-error-page' ? 'active' : ''}>Error 404 Page</Nav.Link>
+              </Nav.Item>
+            </Nav>
 
             {/*
             <nav>
@@ -56,7 +68,7 @@ function App() {
               <Route path="*" element={<NotFound />} />
               {/* Wildcard route for 404 Not Found */}
             </Routes>
-        </Router>
+        </HashRouter>
           <Navbar expand="lg" bg="dark" data-bs-theme="dark" className="bg-body-tertiary p-4">
             <h5 className="text-center text-white">&copy; CounterApp 2023 | Designed by: </h5>
           </Navbar>
